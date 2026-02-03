058f9a232e16d0f566ded6020d95ae9e2799618d
OfficialAaronVelez <aaronvelezcoronadosignup@gmail.com>
2026-02-03 16:35:38 -0700

    pagina de registro email

--- app/src/main/java/com/example/meetly/OpcionesLogin.kt
+++ app/src/main/java/com/example/meetly/OpcionesLogin.kt
@@ -1,11 +1,8 @@
-package com.example.meetly
+package com.example.meetly.com.example.meetly
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.meetly.Opciones_Login.Login_email
 import com.example.meetly.databinding.ActivityOpcionesLoginBinding
 
