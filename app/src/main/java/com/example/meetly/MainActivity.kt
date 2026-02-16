a247ec84d156a65a38af154802074d6c65309bea
Naraka28 <eildan28@hotmail.com>
2026-02-16 12:24:25 -0700

    feat(logout) add logout

--- app/src/main/java/com/example/meetly/MainActivity.kt
+++ app/src/main/java/com/example/meetly/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.meetly
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.meetly.Fragmentos.FragmentAccount
+import com.example.meetly.Fragmentos.FragmentHome
+import com.example.meetly.com.example.meetly.OpcionesLogin
 import com.example.meetly.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        comprobarSesion()
+
+        verFragmentHome()
 
         binding.BottomNV.setOnItemSelectedListener { item ->
             when(item.itemId){
@@ -23,6 +35,7 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.Item_Account -> {
+                    verFragmentCuenta()
                     true
                 }
                 else -> {
@@ -31,5 +44,26 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+    private fun verFragmentHome(){
+        binding.TituloRL.text = "Inicio"
+        val fragment = FragmentHome()
+        val fragmenteTransition = supportFragmentManager.beginTransaction()
+        fragmenteTransition.replace(binding.FragmentL1.id, fragment, "FragmentInicio")
+        fragmenteTransition.commit()
+    }
+    private fun comprobarSesion(){
+        if(firebaseAuth.currentUser == null){
+            startActivity(Intent(this, OpcionesLogin::class.java))
+            finishAffinity()
+        }
+
+    }
+    private fun verFragmentCuenta(){
+        binding.TituloRL.text = "Cuenta"
+        val fragment = FragmentAccount()
+        val fragmenteTransition = supportFragmentManager.beginTransaction()
+        fragmenteTransition.replace(binding.FragmentL1.id, fragment, "FragmentCuenta")
+        fragmenteTransition.commit()
     }
 }
--- app/src/main/java/com/example/meetly/OpcionesLogin.kt
+++ app/src/main/java/com/example/meetly/OpcionesLogin.kt
@@ -3,19 +3,32 @@ package com.example.meetly.com.example.meetly
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.meetly.MainActivity
 import com.example.meetly.Opciones_Login.Login_email
 import com.example.meetly.databinding.ActivityOpcionesLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class OpcionesLogin : AppCompatActivity() {
     private lateinit var binding: ActivityOpcionesLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        comprobarSesion()
+
+
         binding.IngresarEmail.setOnClickListener {
             startActivity(Intent(this@OpcionesLogin, Login_email::class.java))
+        }
+    }
+    private fun comprobarSesion(){
+        if(firebaseAuth.currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finishAffinity()
         }
     }
 }
